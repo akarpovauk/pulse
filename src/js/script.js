dec0d27588d8128df56b1368f7f12cc0fd5f7a0f
Alena <alena.karpov@gmail.com>
2021-06-30 14:40:40 +0100

    footer, modal sections, js for modals

--- src/js/script.js
+++ src/js/script.js
@@ -44,4 +44,22 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link_forward');
     toggleSlide('.catalog-item__link_back');
+
+    //modal
+    $('[data-modal=consultation]').on('click',  function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
+
+
+
